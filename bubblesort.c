c26b37a9eec6ebaaf23e1645bacbfe24e03a0f8b
Gorbach <43406162+WereBudLUL@users.noreply.github.com>
2019-01-08 14:53:47 +0100

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+ int zahlen[] = {0 , 8 , 3 , 5 };
+ int placeholder, length, i, j, k;
+ 
+ while(zahlen[k])
+ {
+     k++;
+     length++;
+ }
+ 
+ printf("%d", length);
+ 
+}
